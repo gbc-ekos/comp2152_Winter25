8058bf538ffe153683d77fc7bfc6c8aefc309e34
Eduard Kosenko <eduard.kosenko@georgebrown.ca>
2025-01-14 10:15:47 -0500

    add elements array

---
+++ Week02/lab2.py
@@ -0,0 +1,8 @@
+import random
+
+elements = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon']
+print("Elements: ", elements)
+# weapons = ['Fist', 'Knife', 'Club', 'Bomb', 'Nuclear bomb']
+
+# weaponRoll = random.randint(1, 6)
+
